59d511d59619d792b0b71289ec1a0eebc369f3eb
jf-759 <jane.fernandez@students.dominican.edu>
2024-11-15 13:24:41 -0800

    completed ability.py

---
+++ ability.py
@@ -0,0 +1,34 @@
+# ability.py
+import random
+
+class Ability:
+    def __init__(self, name, max_damage):
+        '''
+        Initialize the values passed into this method as instance variables.
+        '''
+
+        # Assign the 'name' and 'max_damage'
+        # for a specific instance of the Ability class
+        self.name = name
+        self.max_damage = max_damage
+
+    def attack(self):
+        ''' 
+        Return a value between 0 and the value set by self.max_damage.
+        '''
+
+        # pick a random value between 0 and self.max_damage
+        random_value = random.randint(0, self.max_damage)
+        return random_value
+    
+if __name__ == '__main__':
+    
+    ability1 = Ability('Web Shooter', 10)
+    ability2 = Ability('Thunderstruck', 20)
+
+    print(ability1.name)
+    print(ability1.attack())
+
+    print(ability2.name)
+    print(ability2.attack())
+
--- hero.py
+++ hero.py
@@ -11,48 +11,20 @@ class Hero:
         starting_health: Integer
         current_health: Integer
         '''
-    # we know the name of our hero, so we assign it here.
-    # make sure it is indented so it stays inside the method.
         self.name = name
-    # similarly, our starting health is passed in.
         self.starting_health = starting_health
-    # when a hero is created, their current health is
-    # always the same as their starting health (no damage yet!)
         self.current_health = starting_health
 
     # fight method:
     def fight(self, opponent):
         ''' Current Hero will take turns fighting the opponent hero passed '''
-        # TODO: fight each hero until a victor emerges.
-        # winner = random.choice([self, opponent])
 
-        # if winner == self:
-        #     print(f'{self.name} wins the fight against {opponent.name}!')
-        # else:
-        #     print(f'{opponent.name} wins the fight against {self.name}!')
-
-        # Phases to implement:
-        # 1. randomly choose winner,
-        # hint: look into random library, more specifically the choice method
-
-        if self.current_health > opponent.current_health:
+        if self.starting_health > opponent.starting_health:
             print(f'{self.name} wins the fight against {opponent.name}!')
             
         else:
-            self.current_health < opponent.current_health
+            self.starting_health < opponent.starting_health
             print(f'{opponent.name} wins the fight against {self.name}!')
-
-
-# note: because we gave the hero a starting health of 200, it shows up as 200
-# even though the default was 100 in the __init__ method.
-# if __name__ == '__main__':
-#     my_hero = Hero("Grace Hopper", 200)
-#     print(my_hero.name)
-#     print(my_hero.current_health)
-# another note: this block will only run if this script is called directly.
-# this will not run if the script is being imported by another script.
-# later on, we will want to test our code, in a different file with the Hero import,
-# but we won't want to test this code.
 
 if __name__ == "__main__":
     hero1 = Hero("Spiderman", 400)
