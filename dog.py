27b92eee9a51b2dac7564f444acd0c5ff213ccde
jf-759 <jane.fernandez@students.dominican.edu>
2024-11-15 11:56:45 -0800

    completed tutorial in P00

---
+++ dog.py
@@ -0,0 +1,24 @@
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        print('dog initialized!')
+
+    # methods are defined as their own named functions inside the class
+    # everytime you make a class method, input the 'self' parameter
+
+    def bark(self):
+        print('Woof!')
+
+
+# instsantiation call that creates a Dog object:
+# --> Dog('Rex') <--
+
+# assigning it to the value of the my_dog variable.
+
+
+# print(my_dog) # <--- This will print <__main__.Dog object at 0x1011d6bd0> which is its dog class.
+# print(my_dog.name) # this will print its name 'Rex'
+
+# should print 'SuperDog'
+# print(my_dog.breed)
---
+++ my_dogs.py
@@ -0,0 +1,8 @@
+import dog
+
+# specify exactly what we want:
+my_dog = dog.Dog('Rex', 'SuperDog')
+my_dog.bark()
+
+my_other_dog = dog.Dog('Annie', 'SuperDog')
+print(my_other_dog.name)
