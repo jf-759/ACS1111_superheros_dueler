521842dedf5dcb7c6d6f7aec9080274bda1327e1
jf-759 <jane.fernandez@students.dominican.edu>
2024-11-18 12:20:57 -0800

    completed create a hero for your arena

--- arena.py
+++ arena.py
@@ -48,5 +48,23 @@ class Arena:
 
         return Armor(name, max_damage)
     
-    
+    def create_hero(self):
+        '''
+        Prompt user for Hero information
+        return Hero with values from user input.
+        '''
+
+        hero_name = input('Hero\'s name: ')
+        hero = Hero(hero_name)
+        add_item = None
+        while add_item !='4':
+            add_item = input('[1] Add ability\n [2] Add weapon\n [3] Add armor\n [4] Done adding items\n\n Your choice: ')
+            if add_item == '1':
+                hero.add_ability(self.create_ability())
+            elif add_item == '2':  
+                hero.add_weapon(self.create_weapon())
+            elif add_item == '3':
+                hero.add_armor(self.create_armor())
+        return hero
         
+    
