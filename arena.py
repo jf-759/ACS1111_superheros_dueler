ff2507732cbf5244d2c481107d28250a02e7f046
jf-759 <jane.fernandez@students.dominican.edu>
2024-11-18 12:11:00 -0800

    added class Arena & prompted ability

---
+++ arena.py
@@ -0,0 +1,30 @@
+from ability import Ability
+from weapon import Weapon
+from armor import Armor
+from hero import Hero
+from team import Team
+
+class Arena:
+    def __init__(self):
+        '''
+        Instantiate properties
+        team_one: None
+        team_two: None
+        '''
+
+        self.team_one = []
+        self.team_two = []
+
+    def create_ability(self):
+        '''
+        Prompt for Ability information.
+        return Ability with values from user Input
+        '''
+
+        name = input('What was the ability name? ')
+        max_damage = input('What is the max damage of the ability? ')
+
+        return Ability(name, max_damage)
+    
+    
+        
